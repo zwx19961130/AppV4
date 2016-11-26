4ae1b965f69e51ff8778ae3d8e23c20860a037f9
luo <17801016258@163.com>
2016-11-26 14:06:19 +0800

    luo7comiit

--- app/src/main/java/com/example/n404/myapplication_luo_2/GLoabalTools/GlobalClientGoodList.java
+++ app/src/main/java/com/example/n404/myapplication_luo_2/GLoabalTools/GlobalClientGoodList.java
@@ -3,7 +3,7 @@ package com.example.n404.myapplication_luo_2.GLoabalTools;
 /**
  * Created by luo on 16-11-26.
  * 这个类就是最直接获取当前内存最新的客户端商品列表数据
- *      
+ *
  */
 
 public class GlobalClientGoodList {
